53f264669d8a8dbde9030fe9d0b3eb2be40a4a1c
Alexandre Urquiza de Sá Filho <118024611+UrquizaFilho@users.noreply.github.com>
2022-12-28 23:54:28 -0300

    Update main.js
    Retirando o estilo de sombreamento na opção de skill

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,22 @@
-/*=============== FILTERS TABS ===============*/
-const tabs = document.querySelectorAll('[data-target]'),
-      tabContents = document.querySelectorAll('[data-content]')
+// /*=============== FILTERS TABS ===============*/
+// const tabs = document.querySelectorAll('[data-target]'),
+//       tabContents = document.querySelectorAll('[data-content]')
 
-tabs.forEach(tab =>{
-    tab.addEventListener('click', () =>{
-        const target = document.querySelectorAll(tab.dataset.target)
+// tabs.forEach(tab =>{
+//     tab.addEventListener('click', () =>{
+//         const target = document.querySelectorAll(tab.dataset.target)
 
-        tabContents.forEach(tc => {
-            tc.classList.remove('filters__active')
-        })
-        target.classList.add('filters__active')
+//         tabContents.forEach(tc => {
+//             tc.classList.remove('filters__active')
+//         })
+//         target.classList.add('filters__active')
 
-        tabs.forEach(t =>{
-            t.classList.remove('filter-tab-active')
-        })
-        tab.classList.add('filter-tab-active')
-    })
-})      
+//         tabs.forEach(t =>{
+//             t.classList.remove('filter-tab-active')
+//         })
+//         tab.classList.add('filter-tab-active')
+//     })
+// })      
 
 /*=============== DARK LIGHT THEME ===============*/
 
